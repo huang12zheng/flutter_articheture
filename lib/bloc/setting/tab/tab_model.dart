ac07c8416cf7a84125dc0d1066768b46930973ef
黄政 <huangzheng@huangzhengdeMBP.lan>
2019-11-13 15:09:30 +0800

    merge

---
+++ lib/bloc/setting/tab/tab_model.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TabItemDM {
+  final String text;
+  final IconData defaultIcon;
+  // final IconData activeIcon;
+
+  // const TabItemDM(this.text, this.defaultIcon, this.activeIcon);
+  const TabItemDM(this.text, this.defaultIcon);
+}
+
+class TabItemData{
+  TabItemData._();
+  static const TabItemDM add =  TabItemDM('add',Icons.add);
+  static const TabItemDM list = TabItemDM('list',Icons.list);
+  static const TabItemDM compare_arrows = TabItemDM('compare_arrows',Icons.compare_arrows);
+
+  static Map<String,TabItemDM>mapToData= {
+    add.text    :   add,
+    list.text   :   list,
+    compare_arrows.text:  compare_arrows,
+
+  };
+}
